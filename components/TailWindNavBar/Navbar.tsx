24f96984d0add30d4e630aae6782a69be6b4ec2f
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro-2.local>
2022-08-26 03:59:28 -0400

    Recreate Navbar using styled components step1 create Span

--- components/TailWindNavBar/Navbar.tsx
+++ components/TailWindNavBar/Navbar.tsx
@@ -1,7 +1,6 @@
 import { MobileNav } from "./MobileNav";
 import { NavLink } from "./NavLink";
 import { useState } from 'react';
-import styled, {css} from 'styled-components';
 
 type BackgroundColour = 'bg-slate-300'|'bg-orange-600'|'bg-green-500';
 type TextColour =  'text-black'|'text-red-200';
@@ -11,43 +10,6 @@ export type NavBarProps = {
     bgColour: BackgroundColour;
     textColour: TextColour;
 }
-
-const baseSpan = css`
-background-color: rgb(0 0 0);
-height: 0.25rem; /* 4px */
-width: 100%;
-border-radius: 0.5rem; /* 8px */
-transition-property: transform;
-transition-duration: 300ms;
-transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-`
-const top = css`
-transform: ${props => props.open && "translateY(0.875rem) rotate(45deg)"};
-`;
-
-const middle = css`
-width: ${props => props.open ? "0%": "100%"};
-`;
-
-const bottom = css`
-transform: ${props => props.open && "translateY(-0.875rem) rotate(-45deg)"};
-`;
-
-const TopSpan = styled.span`
-${baseSpan}
-${top}
-`
-
-const MidSpan = styled.span`
-${baseSpan}
-${middle}
-`
-
-const BottomSpan = styled.span`
-${baseSpan}
-${bottom}
-`
-
 export default function Navbar({bgColour, textColour}:NavBarProps) {
     const [open, setOpen] = useState(false);
     
@@ -63,9 +25,9 @@ export default function Navbar({bgColour, textColour}:NavBarProps) {
                     setOpen(!open)
                 }}>
                     {/* hamburger button */}
-                    <TopSpan open={open} />
-                    <MidSpan open={open} />
-                    <BottomSpan open={open} />
+                    <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                    <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                    <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
                 </div>
 
                 <div className="hidden md:flex">
